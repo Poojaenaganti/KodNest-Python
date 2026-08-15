dcee6b1f6f45c194218e65c52f5a9f4dbe645bdd
Poojaenaganti <poojaenaganti3@gmail.com>
2026-08-15 14:30:35 +0530

    numbersequence

---
+++ track/numbersequence.py
@@ -0,0 +1,12 @@
+# Read the number and word
+n = int(input())
+word = input()
+
+
+print("Numbers:")
+for i in range(1, n+1):
+    print(i)
+
+print("Characters:")
+for char in word:
+    print(char)
