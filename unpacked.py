b96cebcc46ce020e741732afd19e0d98d333c75a
Poojaenaganti <poojaenaganti3@gmail.com>
2026-08-15 14:51:47 +0530

    unpacked

---
+++ unpacked.py
@@ -0,0 +1,14 @@
+name = input()
+course = input()
+score = int(input())
+
+
+student = (name, course, score)
+
+
+n, c, s = student
+
+
+print(f"Name: {n}")
+print(f"Course: {c}")
+print(f"Score: {s}")
