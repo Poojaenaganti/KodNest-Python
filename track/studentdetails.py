aa22d2767e05837c06e431ba595e0d18ea34e25d
Poojaenaganti <poojaenaganti3@gmail.com>
2026-08-15 14:05:40 +0530

    studentdetails

---
+++ track/studentdetails.py
@@ -0,0 +1,14 @@
+# Read and convert the student details
+student = input()
+Age = int(input())
+Rating = float(input())
+
+# Display the values
+print(f"Student: {student}")
+print(f"Age: {Age}")
+print(f"Rating: {Rating}")
+
+# Display the data types
+print(type(student))
+print(type(Age))
+print(type(Rating))
